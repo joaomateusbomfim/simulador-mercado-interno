f70c194d7dd8ea9099ca452ae81e87cee34720fd
joaomateusbomfim <98532761+joaomateusbomfim@users.noreply.github.com>
2023-06-29 13:44:07 -0300

    teste sem 1 listener

--- script.js
+++ script.js
@@ -1,56 +1,55 @@
-document.getElementById('calculador-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    var quantity = parseFloat(document.getElementById('quantity').value);
-    var price = parseFloat(document.getElementById('price').value);
-    var cooperative = parseFloat(document.getElementById('cooperative').value);
-    var funrural = document.getElementById('funrural').checked;
-  
-    var totalBruto = price * quantity * 2.2046 * 1000;
-  
-    if (funrural) {
-      totalBruto += totalBruto * 0.02; // Acrescenta 2% se Funrural estiver marcado
-    }
-  
-    // Envia mensagem pelo WhatsApp
-    var message = 'O valor total bruto é de: $' + totalBruto.toFixed(2);
-    // sendWhatsAppMessage(whatsapp, message);
-    console.log(message);
-    console.log(`Quantidade: `, quantity);
-    console.log(`Preço em Dólar: `, price);
-    console.log(`Cooperativa: `, cooperative);
-    console.log(`Tem funrural: `, funrural);
+/* document.getElementById('calculador-form').addEventListener('submit', function (event) {
+  event.preventDefault();
 
-    
-    
-    // abrir página para confirmar recebimento
-    
-    /*
-    
-    */
-    // Limpa o formulário
-    document.getElementById('calculador-form').reset();
-  });
-  
-  function sendWhatsAppMessage(number, message) {
-    // Lógica para enviar mensagem pelo WhatsApp usando o número fornecido
-    // Esta parte depende da API ou biblioteca que você esteja usando para enviar mensagens pelo WhatsApp
-    // Aqui está apenas um exemplo de como poderia ser feito:
-    console.log('Enviando mensagem para o número', number, ':', message);
+  var quantity = parseFloat(document.getElementById('quantity').value);
+  var price = parseFloat(document.getElementById('price').value);
+  var cooperative = parseFloat(document.getElementById('cooperative').value);
+  var funrural = document.getElementById('funrural').checked;
+
+  var totalBruto = price * quantity * 2.2046 * 1000;
+
+  if (funrural) {
+    totalBruto += totalBruto * 0.02; // Acrescenta 2% se Funrural estiver marcado
   }
-  /*
-  document.querySelector('button[type="submit"]').addEventListener('click', function() {
-    window.open('enviado.html');
-  });
-  */
-  document.querySelector('button[type="submit"]').addEventListener('click', function() {
-    var quantity = parseFloat(document.getElementById('quantity').value);
-    var price = parseFloat(document.getElementById('price').value);
-    var cooperative = parseFloat(document.getElementById('cooperative').value);
-    var funrural = document.getElementById('funrural').checked;
+
+  // Envia mensagem pelo WhatsApp
+  var message = 'O valor total bruto é de: $' + totalBruto.toFixed(2);
+  // sendWhatsAppMessage(whatsapp, message);
+  console.log(message);
+  console.log(`Quantidade: `, quantity);
+  console.log(`Preço em Dólar: `, price);
+  console.log(`Cooperativa: `, cooperative);
+  console.log(`Tem funrural: `, funrural);
   
-    var totalBruto = price * quantity * 2.2046 * 1000;
-    var message = 'O valor total bruto é de: $' + totalBruto.toFixed(2);
-    var url = `enviado.html?message=${encodeURIComponent(message)}`;
-   window.open(url, '_blank');
- });
+
+
+  document.getElementById('calculador-form').reset();
+});
+*/
+
+function sendWhatsAppMessage(number, message) {
+  // Lógica para enviar mensagem pelo WhatsApp usando o número fornecido
+  // Esta parte depende da API ou biblioteca que você esteja usando para enviar mensagens pelo WhatsApp
+  // Aqui está apenas um exemplo de como poderia ser feito:
+  console.log('Enviando mensagem para o número', number, ':', message);
+}
+
+document.querySelector('button[type="submit"]').addEventListener('click', function () {
+  var quantity = parseFloat(document.getElementById('quantity').value);
+  var price = parseFloat(document.getElementById('price').value);
+  var cooperative = parseFloat(document.getElementById('cooperative').value);
+  var funrural = document.getElementById('funrural').checked;
+
+  var totalBruto = price * quantity * 2.2046 * 1000;
+  var message = 'O valor total bruto é de: $' + totalBruto.toFixed(2);
+  var url = `enviado.html?message=${encodeURIComponent(message)}`;
+  window.open(url, '_blank');
+
+  console.log(message);
+  console.log(`Quantidade: `, quantity);
+  console.log(`Preço em Dólar: `, price);
+  console.log(`Cooperativa: `, cooperative);
+  console.log(`Tem funrural: `, funrural);
+
+  document.getElementById('calculador-form').reset();
+});
